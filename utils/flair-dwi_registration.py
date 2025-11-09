e40f05e3586ed228ac5835d6cf873bae43fc8cdb
oussamazouhry <zouhrydoussama@gmail.com>
2025-11-09 20:14:55 +0100

    add dataloading via monai

---
+++ utils/flair-dwi_registration.py
@@ -0,0 +1,41 @@
+# source: https://github.com/pashtari/factorizer-isles22/blob/master/flair-dwi_registration.py
+
+from argparse import ArgumentParser, Namespace
+import os
+import SimpleITK as sitk
+
+
+def main(args: Namespace):
+    root_dir = args.path
+    for i in range(3, 251):
+        id_ = f"sub-strokecase{i:04d}"
+        print(f"id: {id_} done.")
+
+        dwi_path = os.path.join(root_dir, f"{id_}/ses-0001/dwi/{id_}_ses-0001_dwi.nii.gz")
+        fixed_image = sitk.ReadImage(dwi_path)
+
+        flair_path = os.path.join(
+            root_dir,
+            f"{id_}/ses-0001/anat/{id_}_ses-0001_flair.nii.gz",
+        )
+        moving_image = sitk.ReadImage(flair_path)
+
+        registered_image = sitk.Elastix(fixed_image, moving_image)
+        flair_registered_path = os.path.join(
+            root_dir,
+            f"{id_}/ses-0001/anat/{id_}_ses-0001_flair_registered.nii.gz",
+        )
+        assert fixed_image.GetSize() == registered_image.GetSize()
+        sitk.WriteImage(registered_image, flair_registered_path)
+
+
+def get_args() -> Namespace:
+    parser = ArgumentParser(description="""flair-DWI registration.""", add_help=False)
+    parser.add_argument("path")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
